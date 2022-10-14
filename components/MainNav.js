704d9e3e958804bf4eb87269be96bb0e786cbdb5
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-10-13 20:43:43 -0400

    feature: global SWR config added to '_app.js' file that Next.js uses to initialize each page
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
 export default function MainNav(props) {
   return (
     <>
-      <p>MainNav</p>
+      {/* <Navbar className='fixed-top navbar-dark bg-dark'></Navbar> {/* same as "NavBar" component below *\/} */}
+      <Navbar className='fixed-top navbar-dark bg-dark' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='/'>Muhammad Ahmed</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Link href='/' passHref>
+              <Nav.Link>Movies</Nav.Link>
+            </Link>
+            <Link href='/about' passHref>
+              <Nav.Link>About</Nav.Link>
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <br />
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { SWRConfig } from 'swr';
+
+import Layout from '../components/Layout';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <SWRConfig value={{ fetcher: (...args) => fetch(...args).then((res) => res.json()) }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </Layout>
+  );
 }
 
 export default MyApp;
