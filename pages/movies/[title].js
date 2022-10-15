7d95e45dbd41175ffdba26fd6aa077bcdb5f5945
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-10-14 22:56:57 -0400

    feature: created movie query by title route, '/movies/:title'
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- pages/movies/[title].js
+++ pages/movies/[title].js
@@ -1,7 +1,32 @@
+import useSWR from 'swr';
+
+import { useRouter } from 'next/router';
+import Error from 'next/error';
+import MovieDetails from '../../components/MovieDetails';
+import PageHeader from '../../components/PageHeader';
+
 export default function Movie(props) {
+  const router = useRouter();
+
+  const api = `https://movie-api.cyclic.app`;
+  const { title } = router.query;
+  const urlRequest = `${api}/api/movies?page=1&perPage=10&title=${title}`;
+  const { data, error } = useSWR(urlRequest);
+
   return (
     <>
-      <p>Movie by Title</p>
+      {data ? (
+        data.length > 0 ? (
+          data.map((movie) => (
+            <div key={movie._id}>
+              <PageHeader text={movie.title} />
+              <MovieDetails movie={movie} />
+            </div>
+          ))
+        ) : (
+          <p>hello</p>
+        )
+      ) : null}
     </>
   );
 }
